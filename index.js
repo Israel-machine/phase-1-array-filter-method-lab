81df1190c11f5c12bf0629b89e3ed600abd34c71
israel diaz <israel.diaz1701@gmail.com>
2023-06-18 17:35:15 -0700

    Final Array Filter Lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+function findMatching(driverArray,string) {
+    // console.log("drivers",driverArray)
+    // console.log("string",string)
+
+    let filteredNames =  driverArray.filter(function(name) {
+        // console.log("name",name)
+        return name.toUpperCase() === string.toUpperCase();
+    })
+
+    // console.log("filteredNames", filteredNames)
+    
+    return filteredNames
+}
+
+function fuzzyMatch(driverArray,string) {
+    let fuzzyFilter = driverArray.filter(function(word) {
+        // console.log({word,string})
+        return word.indexOf(string) === 0;
+    })
+    return fuzzyFilter;
+};
+
+function matchName(driversArray,string) {
+    // console.log(driversArray, string)
+    let matchFilter = driversArray.filter(function(driver) {
+        console.log(driver)
+        return driver.name === string
+    })
+    return matchFilter
+ }
